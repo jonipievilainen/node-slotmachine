a15a0e6a97bf574da7bb77b1032564e26be3db30
Pievi <r1jo1jo1@gmail.com>
2020-07-05 19:34:23 +0300

    Controller buttons

--- arduino/controller/controller.ino
+++ arduino/controller/controller.ino
@@ -1,33 +1,65 @@
-int pushButton = 3;
+int playLed     = 11;
+int playButton  = 12;
+
+int col1Led     = 9;
+int col1Button  = 10;
+
+int col2Led     = 7;
+int col2Button  = 8;
+
+int col3Led     = 5;
+int col3Button  = 6;
+
+int winLed      = 3;
+int winButton   = 4;
+
+
 
 void setup() {
-  pinMode(5, OUTPUT);
-  pinMode(7, OUTPUT);
+
   Serial.begin(9600);
-  pinMode(pushButton, INPUT);
+
+  pinMode(playLed,    OUTPUT);
+  pinMode(playButton, INPUT);
+  pinMode(col1Led,    OUTPUT);
+  pinMode(col1Button, INPUT);
+  pinMode(col2Led,    OUTPUT);
+  pinMode(col2Button, INPUT);
+  pinMode(col3Led,    OUTPUT);
+  pinMode(col3Button, INPUT);
+  pinMode(winLed,     OUTPUT);
+  pinMode(winButton,  INPUT);
 }
 
 void loop() {
-  int buttonState = digitalRead(pushButton);
+
+
+  int playButtonState = digitalRead(playButton);
+  int col1ButtonState = digitalRead(col1Button);
+  int col2ButtonState = digitalRead(col2Button);
+  int col3ButtonState = digitalRead(col3Button);
+  int winButtonState  = digitalRead(winButton);
   
-  Serial.println(buttonState);
+  Serial.println(playButtonState);
 
   if (Serial.available()) {
     int state = Serial.parseInt();
 
-    // Serial.println(state);
+    Serial.println(state);
 
+    /*
     if (state == 1) {
-      digitalWrite(5, HIGH);
+      digitalWrite(playLed, HIGH);
       delay(500);
-      digitalWrite(5, LOW);
+      digitalWrite(playLed, LOW);
     }
 
     if (state == 2) {
-      digitalWrite(7, HIGH);
+      digitalWrite(col1Led, HIGH);
       delay(500);
-      digitalWrite(7, LOW);
+      digitalWrite(col1Led, LOW);
     }
+    */
   }
   
   delay(100);        // delay in between reads for stability
